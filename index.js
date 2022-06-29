c1e6c2d70b9d9e6aac3b8fb67ed745df67c05b3a
Evan Rosson <rossonevan7281@gmail.com>
2022-06-29 15:41:46 -0500

    commit tree 61dbf5d4fb65cb4aea296cb13de3ef819b8e6604

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(source, sought) {
+    return source.filter(
+        (possibleMatch) => possibleMatch.toLowerCase() === sought.toLowerCase()
+    );
+}
+
+function fuzzyMatch(source, sought) {
+    return source.filter(
+        (possibleMatch) => possibleMatch[0] === sought[0]
+    );
+}
+
+function matchName(source, sought) {
+    return source.filter(
+        (possibleMatch) => possibleMatch.name === sought
+    );
+}
